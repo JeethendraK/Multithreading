5737f94bf1904bd337d71415bd451a7ec6cd5239
JeethendraK <jeethendra.reddy.k@gmail.com>
2025-03-08 21:04:31 +0530

    adding an image

--- src/in/thread/volatileEx/SharedResource.java
+++ src/in/thread/volatileEx/SharedResource.java
@@ -1,12 +1,16 @@
 package in.thread.volatileEx;
 
 public class SharedResource {
+    //using Volatile keyword to make sure that the flag is visible to all threads
+    //private boolean flag = false;
+    //private volatile boolean flag = false;
+
+    //using synchronized keyword to make sure that the flag is visible to all threads
     private boolean flag = false;
-    
-    public void setFlag(boolean flag) {
+    public synchronized void  setFlag(boolean flag) {
         this.flag = flag;
     }
-    public boolean getFlag() {
+    public synchronized boolean getFlag() {
         return flag;
     }
 
--- src/in/thread/volatileEx/Volatile_keyword.java
+++ src/in/thread/volatileEx/Volatile_keyword.java
@@ -2,32 +2,31 @@ package in.thread.volatileEx;
 
 public class Volatile_keyword {
     public static void main(String[] args) {
+        // Create an instance of SharedResource class to be shared between threads
         SharedResource sharedResource = new SharedResource();
 
-        //Thread 1
-        new Thread(() ->{
-            System.out.println("Thread 1 started");
-            try {
-                System.out.println("Thread 1 logic is Started");
-                Thread.sleep(1000);
-                System.out.println("Thread 1 logic is completed");
-                sharedResource.setFlag(true);
-                System.out.println("Thread 1 set flag to true");
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            
-        }).start();
-
-        //Thread 2
+        // Thread 1: Sets the flag after performing some logic
         new Thread(() -> {
-            System.out.println("Thread 2 started");
-            while(!sharedResource.getFlag()){
-
+            System.out.println("Thread 1 started");  // Indicate that thread 1 has started
+            try {
+                System.out.println("Thread 1 logic is Started"); // Indicate logic execution start
+                Thread.sleep(1000);  // Simulate some work by sleeping for 1 second
+                System.out.println("Thread 1 logic is completed");  // Indicate logic execution completion
+                
+                sharedResource.setFlag(true);  // Set the flag to true
+                System.out.println("Thread 1 set flag to true");  // Indicate flag setting
+            } catch (InterruptedException e) {
+                e.printStackTrace();  // Handle potential interrupt exception
             }
-            System.out.println("Thread 2 is completed");
-        }).start();
-    }
+        }).start();  // Start Thread 1
 
+        // Thread 2: Waits for the flag to become true before completing
+        new Thread(() -> {
+            System.out.println("Thread 2 started");  // Indicate that thread 2 has started
+            while (!sharedResource.getFlag()) {
+                // Wait until the flag is true; it will keep looping if the flag is false
+            }
+            System.out.println("Thread 2 is completed");  // Indicate thread 2 completion after flag is true
+        }).start();  // Start Thread 2
+    }
 }
